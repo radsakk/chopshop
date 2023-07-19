707f49a1f5914ebf29caf77e9cd87bd1f7eb79b4
radsakk <lherr019@jaguar.tamu.edu>
2023-07-19 11:41:19 -0500

    moree

--- public/js/footer.js
+++ public/js/footer.js
@@ -3,33 +3,33 @@ const createFooter = () => {
 
     footer.innerHTML = `
     <div class="footer-content">
-        <img src="img/light-logo.png" class="logo" alt="">
+        <img src="" class="logo" alt="">
         <div class="footer-ul-container">
             <ul class="category">
                 <li class="category-title">men</li>
-                <li><a href="#" class="footer-link">t-shirts</a></li>
-                <li><a href="#" class="footer-link">sweatshirts</a></li>
-                <li><a href="#" class="footer-link">shirts</a></li>
-                <li><a href="#" class="footer-link">jeans</a></li>
-                <li><a href="#" class="footer-link">trousers</a></li>
-                <li><a href="#" class="footer-link">shoes</a></li>
-                <li><a href="#" class="footer-link">casuals</a></li>
-                <li><a href="#" class="footer-link">formals</a></li>
-                <li><a href="#" class="footer-link">sports</a></li>
-                <li><a href="#" class="footer-link">watch</a></li>
+                <li><a href="#" class="footer-link">t</a></li>
+                <li><a href="#" class="footer-link">s</a></li>
+                <li><a href="#" class="footer-link">i</a></li>
+                <li><a href="#" class="footer-link">js</a></li>
+                <li><a href="#" class="footer-link">r</a></li>
+                <li><a href="#" class="footer-link">o</a></li>
+                <li><a href="#" class="footer-link">c</a></li>
+                <li><a href="#" class="footer-link">f</a></li>
+                <li><a href="#" class="footer-link">p</a></li>
+                <li><a href="#" class="footer-link">w</a></li>
             </ul>
             <ul class="category">
                 <li class="category-title">women</li>
                 <li><a href="#" class="footer-link">t-shirts</a></li>
-                <li><a href="#" class="footer-link">sweatshirts</a></li>
-                <li><a href="#" class="footer-link">shirts</a></li>
-                <li><a href="#" class="footer-link">jeans</a></li>
-                <li><a href="#" class="footer-link">trousers</a></li>
-                <li><a href="#" class="footer-link">shoes</a></li>
-                <li><a href="#" class="footer-link">casuals</a></li>
-                <li><a href="#" class="footer-link">formals</a></li>
-                <li><a href="#" class="footer-link">sports</a></li>
-                <li><a href="#" class="footer-link">watch</a></li>
+                <li><a href="#" class="footer-link">e</a></li>
+                <li><a href="#" class="footer-link">i</a></li>
+                <li><a href="#" class="footer-link">j</a></li>
+                <li><a href="#" class="footer-link">r</a></li>
+                <li><a href="#" class="footer-link">h</a></li>
+                <li><a href="#" class="footer-link">c</a></li>
+                <li><a href="#" class="footer-link">f</a></li>
+                <li><a href="#" class="footer-link">p</a></li>
+                <li><a href="#" class="footer-link">w</a></li>
             </ul>
         </div>
     </div>
