14ffcd132448d00cd08561f7502b7c0fb8df078e
radsakk <lherr019@jaguar.tamu.edu>
2023-07-23 13:15:07 -0500

    yeet

--- public/js/footer.js
+++ public/js/footer.js
@@ -3,39 +3,25 @@ const createFooter = () => {
 
     footer.innerHTML = `
     <div class="footer-content">
-        <img src="" class="logo" alt="">
+        <img src="img/dark-logo.png" class="logo" alt="">
         <div class="footer-ul-container">
             <ul class="category">
-                <li class="category-title">men</li>
-                <li><a href="#" class="footer-link">t</a></li>
-                <li><a href="#" class="footer-link">s</a></li>
-                <li><a href="#" class="footer-link">i</a></li>
-                <li><a href="#" class="footer-link">js</a></li>
-                <li><a href="#" class="footer-link">r</a></li>
-                <li><a href="#" class="footer-link">o</a></li>
-                <li><a href="#" class="footer-link">c</a></li>
-                <li><a href="#" class="footer-link">f</a></li>
-                <li><a href="#" class="footer-link">p</a></li>
-                <li><a href="#" class="footer-link">w</a></li>
+                <li class="category-title">Best Sellers</li>
+                <li><a href="best.html" class="footer-link">Click Here</a></li>            
+            <ul class="category">
+                <li class="category-title">Discounts</li>
+                <li><a href="discount.html" class="footer-link">Click Here</a></li>                
             </ul>
             <ul class="category">
-                <li class="category-title">women</li>
-                <li><a href="#" class="footer-link">t-shirts</a></li>
-                <li><a href="#" class="footer-link">e</a></li>
-                <li><a href="#" class="footer-link">i</a></li>
-                <li><a href="#" class="footer-link">j</a></li>
-                <li><a href="#" class="footer-link">r</a></li>
-                <li><a href="#" class="footer-link">h</a></li>
-                <li><a href="#" class="footer-link">c</a></li>
-                <li><a href="#" class="footer-link">f</a></li>
-                <li><a href="#" class="footer-link">p</a></li>
-                <li><a href="#" class="footer-link">w</a></li>
-            </ul>
+                <li class="category-title">Products</li>
+                <li><a href="product.html" class="footer-link">Click Here</a></li>                
+            </ul>            
         </div>
     </div>
-    <p class="footer-title">about company</p>
-    <p class="info">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat tempore ad suscipit, eos eius quisquam sed optio nisi quaerat fugiat ratione et vero maxime praesentium, architecto minima reiciendis iste quo deserunt assumenda alias ducimus. Ullam odit maxime id voluptates rerum tenetur corporis laboriosam! Cum error ipsum laborum tempore in rerum necessitatibus nostrum nobis modi! Debitis adipisci illum nemo aperiam sed, et accusamus ut officiis. Laborum illo exercitationem quo culpa reprehenderit excepturi distinctio tempore cupiditate praesentium nisi ut iusto, assumenda perferendis facilis voluptas autem fuga sunt ab debitis voluptatum harum eum. Asperiores, natus! Est deserunt incidunt quasi placeat omnis, itaque harum?</p>
-    <p class="info">support emails - help@clothing.com, customersupport@clothing.com</p>
+    <p class="footer-title">About US</p>
+    <p class="info">This movie shop website was made for educational purposes. It was created for CSCI 3354 Web Application Development
+        course in summer of 2023. It features a small variety of movies that are 'forsale'.</p>
+    <p class="info">support emails - help@movies.com, customersupport@movies.com</p>
     <p class="info">telephone - 180 00 00 001, 180 00 00 002</p>
     <div class="footer-social-container">
         <div>
@@ -48,7 +34,7 @@ const createFooter = () => {
             <a href="https://twitter.com/i/flow/login?redirect_after_login=%2Flogin%3Flang%3Den" class="social-link">twitter</a>
         </div>
     </div>
-    <p class="footer-credit">Clothing, Best apparels online store</p>
+    <p class="footer-credit">Movies, Best movie online store</p>
     `;
 }
 
